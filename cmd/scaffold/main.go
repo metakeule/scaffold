b4ecfb506657072b927f5b67763e6814d634f14d
Marc Rene Arns <arns@stridor.net>
2015-01-08 12:18:22 +0100

    fix import paths

--- cmd/scaffold/main.go
+++ cmd/scaffold/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/metakeule/scaffold.v0"
 	"gopkg.in/metakeule/config.v1"
+	"gopkg.in/metakeule/scaffold.v0"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -12,7 +12,7 @@ import (
 var (
 	cfg = config.MustNew("scaffold", "1.0",
 		`scaffold creates files and directories based on a template and json input.
-		Complete documentation at http://godoc.org/github.com/metakeule/scaffold.`)
+		Complete documentation at http://godoc.org/gopkg.in/metakeule/scaffold.v0.`)
 
 	templateArg = cfg.NewString("template", "the file where the template resides", config.Required, config.Shortflag('t'))
 	dirArg      = cfg.NewString("dir", "directory that is the target/root of the file creations", config.Default("."))
